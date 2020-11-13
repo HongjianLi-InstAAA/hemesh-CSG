a123b9f0f8da20bf0f32c4c5e0519dc806cfa98b
Hongjian Li <220160083@seu.edu.com>
2020-11-13 11:00:16 +0800

    update tools to convert from HE_Mesh to CSG

--- src/main/java/main/Geometry.java
+++ src/main/java/main/Geometry.java
@@ -1,6 +1,9 @@
 package main;
 
+import wblut.geom.WB_Coord;
+import wblut.hemesh.HE_Face;
 import wblut.hemesh.HE_Mesh;
+import wblut.hemesh.HE_Vertex;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,14 +23,24 @@ public class Geometry {
     }
 
     public void addMesh(HE_Mesh mesh){
-
+        List<HE_Vertex>vertices = mesh.getVertices();
+        for (HE_Vertex v:vertices)
+            addVerts(v);
+        for (HE_Face f:mesh.getFaces()){
+            List<HE_Vertex>faceVertices = f.getFaceVertices();
+            int[]versID = new int[faceVertices.size()];
+            for (int i = 0; i < versID.length; i++) {
+                versID[i] = vertices.indexOf(faceVertices.get(i));
+            }
+            faces.add(versID);
+        }
     }
 
     public void addVerts(WB_Coord pt) {
         verts.add(new double[]{pt.xd(), pt.yd(), pt.zd()});
     }
 
-    public void addFaces(int f0, int f1, int f2) {
+    public void addFace(int f0, int f1, int f2) {
         faces.add(new int[]{f0, f1, f2});
 
     }
